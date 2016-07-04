7c3e4836f5c9313d53fb5db2d05d39ed994f6fae
Maxi Rosson <maxirosson@gmail.com>
2016-07-04 00:01:45 -0300

    Automatically generate the releaseNotes.txt content. Close #13

--- src/main/groovy/com/jdroid/component/builder/tasks/CreateGitHubReleaseTask.groovy
+++ src/main/groovy/com/jdroid/component/builder/tasks/CreateGitHubReleaseTask.groovy
@@ -19,17 +19,48 @@ public class CreateGitHubReleaseTask extends AbstractGitHubTask {
 		GitHubClient client = createGitHubClient();
 
 		String tagName = "v${project.version}"
-		def releaseNotesFile = project.file("./etc/releaseNotes.txt")
+		String releaseNotes = getReleaseNotes()
 
-		IRepositoryIdProvider repositoryIdProvider = getIRepositoryIdProvider();
-		ReleaseService releaseService = new ReleaseService(client);
-		Release release = releaseService.getReleaseByTagName(repositoryIdProvider, tagName);
+		IRepositoryIdProvider repositoryIdProvider = getIRepositoryIdProvider()
+		ReleaseService releaseService = new ReleaseService(client)
+		Release release = releaseService.listReleases(repositoryIdProvider).find { it.tagName == tagName }
 		if (release == null) {
-			createRelease(client, repositoryIdProvider, tagName, releaseNotesFile.getText());
+			createRelease(releaseService, repositoryIdProvider, tagName, releaseNotes);
 			println 'Verify that the release is present on Releases [https://github.com/' + getRepositoryOwner() + '/' + getRepositoryName() + '/releases]'
 		} else {
 			getLogger().warn('Skipping ' + tagName + ' release creation because it already exists.')
 		}
+	}
+
+	private String getReleaseNotes() {
+		File projectDir = getProjectDirectory()
+
+		execute(['github_changelog_generator', '--unreleased-only', '--no-compare-link', '--no-pull-requests', '--no-pr-wo-labels', '--exclude-labels', 'task', '-t', getGitHubReadToken()], projectDir)
+
+		File changeLogFile = new File(projectDir, "/CHANGELOG.md")
+
+		Boolean started = false
+		Boolean exit = false
+		StringBuilder builder = new StringBuilder()
+		changeLogFile.eachLine {
+			if (!exit) {
+				if (!started && it.startsWith("## [Unreleased")) {
+					started = true
+				} else if (started) {
+					if (it.contains('This Change Log was automatically generated')) {
+						exit = true
+					} else {
+						builder.append(it)
+						builder.append('\n')
+					}
+				}
+			}
+		}
+
+		execute(['git', 'add', '-A'], projectDir)
+		execute(['git', 'stash'], projectDir)
+
+		return builder.toString().trim()
 	}
 
 	private void createRelease(ReleaseService releaseService, IRepositoryIdProvider repositoryIdProvider, String name, String body) throws IOException {
