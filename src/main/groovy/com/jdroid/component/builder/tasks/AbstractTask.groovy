8658c6f1943bb53279a3bc5e00ed89afb274c975
Maxi Rosson <maxirosson@gmail.com>
2017-03-16 05:32:49 -0300

    Added support to log command execution outputs

--- src/main/groovy/com/jdroid/component/builder/tasks/AbstractTask.groovy
+++ src/main/groovy/com/jdroid/component/builder/tasks/AbstractTask.groovy
@@ -1,26 +1,32 @@
 package com.jdroid.component.builder.tasks
 
+import com.jdroid.component.builder.commons.LogOutputStream
 import org.gradle.api.DefaultTask
 import org.gradle.process.ExecResult
+import org.gradle.api.logging.LogLevel;
 
 public class AbstractTask extends DefaultTask {
 
-	public ExecResult execute(def command, def workingDirectory, Boolean ignoreExitValueParam) {
+	public ExecResult execute(def command, def workingDirectory, Boolean logStandardOutput, Boolean ignoreExitValueParam) {
 		StringBuilder builder = new StringBuilder()
 		command.each {
 			builder.append(it)
 			builder.append(" ")
 		}
 		println "Executing command: " + builder.toString()
-		project.exec {
+		ExecResult execResult = project.exec {
 			workingDir workingDirectory
 			commandLine command
 			ignoreExitValue ignoreExitValueParam
+			if (logStandardOutput) {
+				standardOutput new LogOutputStream(logger, LogLevel.ERROR)
+			}
+			errorOutput new LogOutputStream(logger, LogLevel.ERROR)
 		}
 	}
 
 	public ExecResult execute(def command, def workingDirectory) {
-		execute(command, workingDirectory, false)
+		execute(command, workingDirectory, false, false)
 	}
 
 	public String getGitBranch() {
