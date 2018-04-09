7cf27b963d62a9c885057057d226992e55ab51ec
Maxi <maxirosson@gmail.com>
2018-04-09 15:23:48 -0300

    Fixing release command

--- src/main/groovy/com/jdroid/component/builder/tasks/ReleaseJdroidComponentTask.groovy
+++ src/main/groovy/com/jdroid/component/builder/tasks/ReleaseJdroidComponentTask.groovy
@@ -31,8 +31,8 @@ public class ReleaseJdroidComponentTask extends AbstractGitHubTask {
 		execute('git checkout production')
 		execute('git pull')
 
-		execute('./gradlew clean :verifyJdroidTools :closeJdroidGitHubMilestone :createJdroidGitHubRelease :generateJdroidChangelog',
-				 'uploadArchives --refresh-dependencies --stacktrace -PSNAPSHOT=false -PLOCAL_UPLOAD=false',
-				 '-PRELEASE_BUILD_TYPE_ENABLED=true -PRELEASE_FAKE_ENABLED=true -PACCEPT_SNAPSHOT_DEPENDENCIES=false')
+		execute('./gradlew clean :verifyJdroidTools :closeJdroidGitHubMilestone :createJdroidGitHubRelease ' +
+				':generateJdroidChangelog uploadArchives --refresh-dependencies --stacktrace -PSNAPSHOT=false ' +
+				'-PLOCAL_UPLOAD=false -PRELEASE_BUILD_TYPE_ENABLED=true -PRELEASE_FAKE_ENABLED=true -PACCEPT_SNAPSHOT_DEPENDENCIES=false')
 	}
 }
