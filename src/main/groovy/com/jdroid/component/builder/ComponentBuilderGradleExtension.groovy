370deb19d0e558a15e2201181e5b98f53e4c75c8
Maxi Rosson <maxirosson@gmail.com>
2019-08-10 08:39:27 -0300

    Changed github property names

--- src/main/groovy/com/jdroid/component/builder/ComponentBuilderGradleExtension.groovy
+++ src/main/groovy/com/jdroid/component/builder/ComponentBuilderGradleExtension.groovy
@@ -9,26 +9,26 @@ public class ComponentBuilderGradleExtension extends BaseGradleExtension {
 	}
 
 	public String getGitHubWriteToken() {
-		return getProp('CI_GITHUB_WRITE_TOKEN')
+		return getProp('GITHUB_WRITE_TOKEN')
 	}
 	public String getGitHubReadToken() {
-		return getProp('CI_GITHUB_READ_TOKEN')
+		return getProp('GITHUB_READ_TOKEN')
 	}
 
 	public String getRepositoryOwner() {
-		return getProp('JDROID_GITHUB_REPOSITORY_OWNER', 'maxirosson')
+		return getProp('GITHUB_REPOSITORY_OWNER', 'maxirosson')
 	}
 
 	public String getRepositoryName() {
-		return getProp('JDROID_GITHUB_REPOSITORY_NAME')
+		return getProp('GITHUB_REPOSITORY_NAME')
 	}
 
 	public String getGiHubUsername() {
-		return getProp('CI_GITHUB_USER_NAME')
+		return getProp('GITHUB_USER_NAME')
 	}
 
 	public String getGiHubEmail() {
-		return getProp('CI_GITHUB_USER_EMAIL')
+		return getProp('GITHUB_USER_EMAIL')
 	}
 
 	public String getRepositoryCloneUrl() {
