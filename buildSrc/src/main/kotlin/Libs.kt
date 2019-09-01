f370b0b84fba377bd7b8fcfa0a2990700cbf25c5
Maxi Rosson <maxirosson@gmail.com>
2019-09-01 12:13:48 -0300

    Fixed dependency

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,6 +1,6 @@
 object Libs {
     // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
-    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:2.1.0-SNAPSHOT"
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0-SNAPSHOT"
 
     // https://github.com/maxirosson/jdroid-java-github/releases
     const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.0.0"
