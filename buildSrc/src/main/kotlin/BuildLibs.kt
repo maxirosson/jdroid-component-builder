4d226dd9f0a4e2ef35f2c8c5e88207475b7ad371
Maxi <maxirosson@gmail.com>
2019-09-15 12:19:31 -0300

    Upgrade dependencies

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,7 +1,7 @@
 object BuildLibs {
     // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
-    const val jdroid_gradle_project_plugin = "com.jdroidtools:jdroid-gradle-project-plugin:2.2.0"
+    const val jdroid_gradle_project_plugin = "com.jdroidtools:jdroid-gradle-project-plugin:2.3.0"
 
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
-    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.2.0"
+    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.3.0"
 }
