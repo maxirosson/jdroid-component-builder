f484320c4bed6e530d851ac60945601b1baa4ebf
Maxi Rosson <maxirosson@gmail.com>
2020-11-15 13:57:25 -0300

    Remove Java version validation

--- src/main/groovy/com/jdroid/component/builder/config/ProjectConfigVerificationTask.java
+++ src/main/groovy/com/jdroid/component/builder/config/ProjectConfigVerificationTask.java
@@ -3,9 +3,7 @@ package com.jdroid.component.builder.config;
 import com.jdroid.component.builder.tasks.AbstractTask;
 import com.jdroid.java.utils.StreamUtils;
 
-import org.gradle.api.GradleException;
 import org.gradle.api.plugins.JavaBasePlugin;
-import org.gradle.internal.jvm.Jvm;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,8 +12,6 @@ import java.io.FileNotFoundException;
 
 public class ProjectConfigVerificationTask extends AbstractTask {
 
-	private static final String EXPECTED_JAVA_VERSION = "11";
-
 	public ProjectConfigVerificationTask() {
 		setDescription("Validates if the project configuration is up to date");
 		setGroup(JavaBasePlugin.VERIFICATION_GROUP);
@@ -23,12 +19,6 @@ public class ProjectConfigVerificationTask extends AbstractTask {
 
 	@Override
 	protected void onExecute() {
-
-		String javaVersion = Jvm.current().getJavaVersion().toString();
-		if (!javaVersion.equals(propertyResolver.getStringProp("EXPECTED_JAVA_VERSION", EXPECTED_JAVA_VERSION))) {
-			throw new GradleException("Wrong java version. Actual version: " + javaVersion + " | Expected version: " + EXPECTED_JAVA_VERSION);
-		}
-
 		File rootDir = getProject().getRootDir();
 
 		for (ProjectConfig projectConfig : ProjectConfig.values()) {
