efafc10308230754aa98552b09f94acaa997e7d4
Maxi Rosson <maxirosson@gmail.com>
2020-11-19 17:31:59 -0300

    Migrate to com.dipien groupId

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -3,6 +3,6 @@ object BuildLibs {
     const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0"
     const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.4.0"
 
-    // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
-    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.3.0"
+    // https://github.com/dipien/dipien-component-builder/blob/master/CHANGELOG.md
+    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:dipien-component-builder:3.0.0-SNAPSHOT"
 }
