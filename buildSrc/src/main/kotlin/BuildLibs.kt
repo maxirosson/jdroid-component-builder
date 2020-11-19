11ea7532e97fab2ad9b3f285ab464cf1c5102b1e
Maxi Rosson <maxirosson@gmail.com>
2020-11-19 17:43:02 -0300

    Upgrade dependencies

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,7 +1,7 @@
 object BuildLibs {
     // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
-    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0"
-    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.4.0"
+    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.5.0-SNAPSHOT"
+    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.5.0-SNAPSHOT"
 
     // https://github.com/dipien/dipien-component-builder/blob/master/CHANGELOG.md
     const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:dipien-component-builder:3.0.0-SNAPSHOT"
