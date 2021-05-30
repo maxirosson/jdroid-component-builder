f1eb534b9de31056e6e7d043b654a0dcd3f4d99e
dipien-ci <dipien2020@gmail.com>
2021-05-30 01:37:07 +0000

    Upgraded com.jdroidtools:jdroid-java-core from 3.0.0 to 3.1.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,6 +1,6 @@
 object Libs {
     // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
-    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.1.0"
 
     const val junit = "junit:junit:4.12"
 }
